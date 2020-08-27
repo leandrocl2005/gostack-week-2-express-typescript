1c5ba15441619f0fdd4ae5952918e87d0b61dd72
leandrocl2005 <leandrocl2005@yahoo.com.br>
2020-08-27 13:18:31 -0300

    user managment

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '4ee4e58ac10705af1b88515b09c5c619',
+    expiresIn: '1d',
+  },
+};
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -5,7 +5,11 @@ import { getCustomRepository } from 'typeorm';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../service/CreateAppointmentService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.get('/', async (request, response) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
@@ -15,7 +19,8 @@ appointmentsRouter.get('/', async (request, response) => {
 
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    // eslint-disable-next-line camelcase
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
 
@@ -23,7 +28,7 @@ appointmentsRouter.post('/', async (request, response) => {
 
     const appointment = await createAppointment.execute({
       date: parsedDate,
-      provider,
+      provider_id,
     });
 
     return response.json(appointment);
