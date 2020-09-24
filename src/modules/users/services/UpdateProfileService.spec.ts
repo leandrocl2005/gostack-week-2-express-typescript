9cb7a6417994f809410106cb28cfe7adbbacf46c
leandrocl2005 <leandrocl2005@yahoo.com.br>
2020-09-24 08:33:49 -0300

    finish user module

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -100,4 +100,14 @@ describe('UpdateProfile', () => {
       })
     ).rejects.toBeInstanceOf(AppError);
   });
+
+  it('should not be able to update the profile from a non-existing user', async () => {
+    await expect(
+      updateProfile.execute({
+        user_id: 'non-existing-id',
+        name: "John Doe",
+        email: "example@example.com"
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
